983f59593a70512842ba7e08a7e13b51bf38dd56
moorejr5 <james.r.moore@vanderbilt.edu>
2019-09-23 14:25:23 -0500

    Testing.

--- download_pdf.php
+++ download_pdf.php
@@ -34,7 +34,9 @@ if (isset($singleRecord['repeat_instances'])) {
     $subRecord = $singleRecord['repeat_instances'];
     $singleRecord[$event_id] = $subRecord[$event_id][''][$repeat_instance];
 }
-
+echo "<pre>";
+print_r($singleRecord);
+echo "</pre>";
 $currentY = $module->generateCustomTable($pdf,$currentY,array(),$instrument,$formTitle,$theMeta);
 $currentY = $module->generateFormForRecord($pdf,$currentY,$singleRecord,$formMetadata,$fullMetadata,$formTitle,array());
 
