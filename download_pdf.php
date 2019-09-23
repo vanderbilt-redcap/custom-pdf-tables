a2d206e099c687192cda17ce8ea6fdf9e5efc311
moorejr5 <james.r.moore@vanderbilt.edu>
2019-09-23 13:50:33 -0500

    Testing.

--- download_pdf.php
+++ download_pdf.php
@@ -28,7 +28,10 @@ $formTitle = $Proj->forms[$instrument]["menu"];
 $recordData = \REDCap::getData($project_id, 'array', array($record), array(), array($event_id), array(), false, false, false);
 
 $theMeta = $module->processTableSettings($recordData,$project_id, $record, $instrument, $event_id, $group_id, $repeat_instance);
-
+$singleRecord = $recordData[$record];
+if (isset($singleRecord['repeat_instances'])) {
+    $singleRecord = $singleRecord['repeat_instances'][$event_id][][$repeat_instance];
+}
 $currentY = $module->generateCustomTable($pdf,$currentY,array(),$instrument,$formTitle,$theMeta);
 $currentY = $module->generateFormForRecord($pdf,$currentY,$recordData[$record],$formMetadata,$fullMetadata,$formTitle,array());
 
